e298aae4ae57fb8ca6d01df6468a51fa1a7abea5
jinhee <iiojinee@gmail.com>
2024-03-20 11:09:20 +0900

    project init

---
+++ src/main/java/com/dev/webtoonpalette/WebtoonPaletteApplication.java
@@ -0,0 +1,13 @@
+package com.dev.webtoonpalette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtoonPaletteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtoonPaletteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/webtoonpalette/WebtoonPaletteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.webtoonpalette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtoonPaletteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
